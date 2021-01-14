9cbf21bd17c6a7b04edfc36d068f4e189dd39947
Chasel <867966307@qq.com>
2021-01-14 13:56:56 +0800

    order module/get all orders info

--- backend/main.go
+++ backend/main.go
@@ -37,12 +37,22 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	//5.注册控制器
+	//5.1.商品控制器
 	productRepository := repositories.NewProductManager("product", db)
 	productService := services.NewProductService(productRepository)
 	productParty := app.Party("/product")
 	productApp := mvc.New(productParty)
 	productApp.Register(ctx,productService)
 	productApp.Handle(new(controllers.ProductController))
+	//5.2.订单控制器
+	orderRepository := repositories.NewOrderManagerRepository("order",db)
+	orderSevice := services.NewOrderService(orderRepository)
+	//地址（通过域名+此地址访问这个订单控制器）
+	orderParty := app.Party("/order")
+	orderApp := mvc.New(orderParty)
+	orderApp.Register(ctx, orderSevice)
+	orderApp.Handle(new(controllers.OrderController))
+
 
 	//6.启动服务
 	app.Run(
--- datamodels/order.go
+++ datamodels/order.go
@@ -1,1 +1,15 @@
 package datamodels
+
+type Order struct{
+	ID int64 `sql:"ID"`
+	UserId int64 `sql:"userI"`
+	ProductId int64 `sql:"productID"`
+	OrderStatus int64 `sql:"orderStatus"`
+}
+
+const (
+	OrderWait = iota
+	OrderSuccess   //1
+	OrderFailed    //2
+
+)
